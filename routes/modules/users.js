43f4a5bb47bbe4c886cb1aa696a553e58efb30c5
Debbie <debbiecamera0704@gmail.com>
2021-06-12 18:13:28 +0800

    feat: add register feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const user = require('../../models/user')
 const router = express.Router()
+const bcrypt = require('bcryptjs')
 const db = require('../../models')
 const User = db.User
 const Todo = db.Todo
@@ -18,10 +19,33 @@ router.get('/register', (req, res) => {
 })
 
 router.post('/register', (req, res) => {
-  const { name, password, email } = req.body
-  User.create({
-    name, email, password
-  }).then(() => res.redirect('/'))
+  const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '請填寫所有欄位' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼和確認密碼不符合' })
+  }
+
+  if (errors.length) {
+    console.log('欄位有問題')
+    return res.render('register', { name, email, password, confirmPassword })
+  }
+
+  return User.findOne({ where: { email } })
+    .then((user) => {
+      if (user) {
+        console.log('此 Email 已註冊')
+        return res.render('register', { name, email, password, confirmPassword })
+      }
+      return bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(password, salt))
+        .then(hash => User.create({ name, email, password: hash }))
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err))
+    })
 })
 
 router.get('/logout', (req, res) => {
